951531565ffadb9855aac4322201beb16eb239fd
Ramath <ramathmanjitha@gmail.com>
2022-11-12 11:26:13 +0530

    chat from counsilor

---
+++ project_iml/screens/counsilor/chat/ChatList.js
@@ -0,0 +1,208 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import FirebaseDB from '../../../../components/firebaseConfig';
+import ChatListItem from './ChatListItem';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+// ################ view as a vicim's chat list
+
+const staticChat = [
+  {
+    id: '09309490-9476-4ba7-82eb-41ddef3dd88a',
+    expertName: 'Deborah Carr',
+    expertId: 'c7d1c7a3-0d16-40b4-9256-770444109b37',
+    victimName: 'Piyumi',
+    victimId: '3f00d10a-4947-4286-afa1-13431dfc5666',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+    messages: [
+      {
+        role: 'expert',
+        msg: 'good',
+        time: '10.12',
+        id: '7de0c1f5-d431-4212-a632-f4af37033a76',
+      },
+      {
+        role: 'expert',
+        msg: 'ok',
+        time: '10.12',
+        id: '7de0c1f5-d431-4212-a632-f4af37033a75',
+      },
+      {
+        role: 'victim',
+        msg: 'I"m afraid to talk',
+        time: '10.15',
+        id: '7de0c1f5-d431-4212-a632-f4af37033a71',
+      },
+      {
+        role: 'victim',
+        msg: 'help me please',
+        time: '10.16',
+        id: '7de0c1f5-d431-4212-a632-f4af37033a77',
+      },
+      {
+        role: 'expert',
+        msg: 'don"t worry',
+        time: '10.16',
+        id: '7de0c1f5-d431-4212-a632-f4af37033a7231',
+      },
+    ],
+  },
+  {
+    id: 'e4fbbf38-7eea-4392-826d-e3a3c968f096',
+    expertName: 'Kamala Hassan',
+    expertId: '74c9cbf1-9a9a-4917-840d-cf823746387f',
+    victimName: 'Piyumi',
+    victimId: '3f00d10a-4947-4286-afa1-13431dfc5666',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+  },
+  {
+    id: 'e39faefa-fbe3-4ef6-98c7-7ad6fc94b227',
+    expertName: 'John J',
+    expertId: 'f8509f73-9400-48d4-910b-74d38b416fa8',
+    victimName: 'Dabare',
+    victimId: '3f7f2bef-dbbc-433d-a709-ce59b6d71132',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+    messages: [
+      {role: 'expert', msg: 'good', time: '10.12'},
+      {role: 'expert', msg: 'ok', time: '10.12'},
+      {role: 'victim', msg: 'I"m afraid to talk', time: '10.15'},
+      {role: 'victim', msg: 'help me please', time: '10.16'},
+      {role: 'expert', msg: 'don"t worry', time: '10.16'},
+    ],
+  },
+  {
+    id: '77dd5472-8890-4b4e-a5b9-db33eca07ba6',
+    expertName: 'Depp J',
+    expertId: '1bb5a8fb-a50f-4438-b6f4-49fa4dcbefdd',
+    victimName: 'Hansamali',
+    victimId: '5d240969-87f6-476e-b34a-2a6f9fb3cd95',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+  },
+  {
+    id: '485064be-4623-433a-9b8c-5641f8682ae7',
+    expertName: 'John J',
+    expertId: 'f8509f73-9400-48d4-910b-74d38b416fa8',
+    victimName: 'Kamala',
+    victimId: '73239c4c-ae0e-47cb-8bb8-0ab3d3dace25',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+  },
+  {
+    id: '53b2373c-da95-47ad-aa82-b35b9b6994a4',
+    expertName: 'Depp J',
+    expertId: '1bb5a8fb-a50f-4438-b6f4-49fa4dcbefdd',
+    victimName: 'Sanduni',
+    victimId: '005d5a2c-e934-477c-abe9-d09b93bd3cd7',
+    expertMsg: ['good', 'ok', 'this is ok', 'don"t worry'],
+    victimMsg: ['I"m afraid to talk', 'help me please', 'I am leaving bye'],
+  },
+];
+
+const ChatList = ({navigation}) => {
+  const [chatList, setChatList] = useState(staticChat);
+  const [userId, setUserId] = useState('3f00d10a-4947-4286-afa1-13431dfc5666'); //this is victim userId
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    // FirebaseDB
+  }, []);
+
+  return (
+    <View
+      style={{
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+      }}>
+      {/* search - start */}
+
+      <View style={styles.Component20}>
+        <View style={styles.Group357}>
+          {/* add search icon here ------------------------ */}
+          <View
+            style={{
+              justifyContent: 'center',
+              paddingRight: 6
+            }}>
+            <AntDesign
+              name="search1"
+              style={{
+                fontSize: 22,
+                justifyContent: 'center',
+                alignItems: 'center'
+                // padding: 20,
+              }}
+            />
+          </View>
+          <TextInput placeholder="Search ..." onChangeText={setSearchQuery} />
+        </View>
+      </View>
+      {/* search - end */}
+
+      <ScrollView>
+        <View
+          style={{
+            alignItems: 'center',
+          }}>
+          {chatList.map((chat, index) => {
+            if (chat.victimId == userId)
+              return (
+                <>
+                  <ChatListItem key={chat.id} chatData={chat} />
+                  {/* <ChatListItem key={chat.id} chatData={chat} /> */}
+                  {/* <Text>hfgfg</Text> */}
+                </>
+              );
+          })}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Component20: {
+    display: 'flex',
+    flexDirection: 'column',
+    // paddingTop: 11.91,
+    // paddingBottom: 9.91,
+    paddingLeft: 27.91,
+    // paddingRight: 243.91,
+    borderRadius: 15,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    width: '90%',
+    marginTop: 20,
+    marginHorizontal: 5,
+    marginBottom: 40,
+  },
+  Group357: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  CurvedSearch: {
+    width: 30.3,
+    height: 14.55,
+    marginRight: 13,
+  },
+  Txt1067: {
+    fontSize: 10,
+    fontFamily: 'Red Hat Display, sans-serif',
+    fontWeight: '500',
+    letterSpacing: 0.2,
+    color: 'rgba(201,197,196,1)',
+  },
+});
+
+export default ChatList;
---
+++ project_iml/screens/counsilor/chat/ChatListItem.js
@@ -0,0 +1,287 @@
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  ScrollView,
+  Button,
+} from 'react-native';
+import Modal from 'react-native-modal';
+
+export default function ChatListItem({chatData}) {
+  const navigation = useNavigation();
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [visible, setVisible] = React.useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  return (
+    <View style={styles.Component21}>
+      {/* <ScrollView> */}
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('Conversation', {chatDetails: chatData})
+        }>
+        <View style={styles.Group692}>
+          <Image
+            style={styles.Image1}
+            source={{
+              uri: 'https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/zv4xqq7wjck-I13%3A514%3B13%3A513?alt=media&token=88fcd3a4-12ed-43ed-822a-02c8a6a7e47d',
+            }}
+          />
+          <View style={styles.Group592}>
+            <Text style={styles.Txt484}>{chatData.expertName}</Text>
+            <Text style={styles.Txt632}>Advanced dip in Physio</Text>
+          </View>
+
+          <TouchableOpacity onPress={() => toggleModal()}>
+            <View style={styles.Frame20}>
+              <View style={styles.Frame17}>
+                <Text style={styles.Txt694}>View</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+          {/* <View style={styles.Rectangle4191}></View> */}
+        </View>
+      </TouchableOpacity>
+      {/* </ScrollView> */}
+
+      {/* modal - start */}
+      <View style={{flex: 1}}>
+        <Modal
+          isVisible={isModalVisible}
+          style={{
+            alignItems: 'center',
+          }}
+          animationType='fade'
+          >
+          {/* <View style = {{
+            backgroundColor: 'white',
+            width: '80%',
+            height: '60%',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}> */}
+          <View style={styles.Rectangle4191}>
+            <View
+              style={{
+                height: '100%',
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  alignItems: 'center',
+                }}>
+                <Image
+                  style={styles.Image2}
+                  source={{
+                    uri: 'https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/hlt48r66he-52%3A407?alt=media&token=70d67c02-a188-4e69-9567-00b24b04a87a',
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontFamily: 'Red Hat Display, sans-serif',
+                    fontWeight: '700',
+                    letterSpacing: 0.5,
+                    color: 'rgba(26,7,0,1)',
+                    marginBottom: 1,
+                    marginTop: 10
+                  }}>
+                  Deborah Carr
+                </Text>
+                <Text style={{fontSize: 14}}>@DeborahC</Text>
+
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontFamily: 'Red Hat Display, sans-serif',
+                    fontWeight: '600',
+                    letterSpacing: 0.38,
+                    color: 'rgba(26,7,0,1)',
+                    marginTop: 20,
+                  }}>
+                  Qualifications
+                </Text>
+                <Text style={{fontSize: 14}}>Advanced dip in Physio</Text>
+                <Text style={{fontSize: 14}}>
+                  Specialist in sexula well being
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontFamily: 'Red Hat Display, sans-serif',
+                    fontWeight: '600',
+                    letterSpacing: 0.38,
+                    color: 'rgba(26,7,0,1)',
+                    marginTop: 20,
+                  }}>
+                  Age
+                </Text>
+                <Text style={{fontSize: 14}}>34</Text>
+                <Text
+                  style={{
+                    fontSize: 16,
+                    fontFamily: 'Red Hat Display, sans-serif',
+                    fontWeight: '600',
+                    letterSpacing: 0.38,
+                    color: 'rgba(26,7,0,1)',
+                    marginTop: 20,
+                  }}>
+                  Occupation
+                </Text>
+                <Text style={{fontSize: 14}}>Surgeon</Text>
+              </View>
+
+              <View
+                style={{
+                  alignItems: 'flex-end',
+                  marginRight: 20,
+                  marginTop: 20,
+                }}>
+                <TouchableOpacity onPress={toggleModal}>
+                  <View
+                    style={{
+                      paddingVertical: 8,
+                      paddingHorizontal: 26,
+                      borderRadius: 15,
+                      backgroundColor: 'rgba(219,1,255,1)',
+                    }}>
+                    <Text
+                      style={{
+                        color: 'white',
+                        fontFamily: 'Red Hat Display, sans-serif',
+                        fontWeight: '500',
+                      }}>
+                      Ok
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+      {/* modal - end */}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Rectangle4191: {
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    width: '80%',
+    height: '100%',
+    borderRadius: 15,
+  },
+  Component21: {
+    display: 'flex',
+    flexDirection: 'column',
+    // justifyContent: "flex-start",
+    // alignItems: "flex-start",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 15,
+    // backgroundColor: "rgba(255, 255, 255, 1)",
+    // marginHorizontal: 10,
+    width: '95%',
+    // height: '10%',
+    marginVertical: 6,
+    // backgroundColor: 'red'
+  },
+  Group692: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    paddingTop: 10,
+    paddingBottom: 8,
+    paddingLeft: 20,
+    paddingRight: 10,
+    borderRadius: 12,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    shadowColor: 'rgba(70,70,70,0.05)',
+    elevation: 3,
+    shadowOffset: {width: 0, height: 20},
+  },
+  Image1: {
+    width: 65,
+    height: 67,
+    marginRight: 27,
+  },
+  Group592: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: 51,
+  },
+  Txt484: {
+    fontSize: 18,
+    fontFamily: 'Red Hat Display, sans-serif',
+    fontWeight: '700',
+    color: 'rgba(26,7,0,1)',
+    marginBottom: 3,
+  },
+  Txt632: {
+    fontSize: 11,
+    fontFamily: 'Red Hat Display, sans-serif',
+    fontWeight: '600',
+    letterSpacing: 0.22,
+    color: 'rgba(26,7,0,1)',
+  },
+
+  Frame20: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 100,
+    backgroundColor: 'rgba(251,224,255,1)',
+    width: 57,
+    height: 26,
+  },
+  Frame19: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingTop: 9,
+    paddingBottom: 9,
+    paddingLeft: 9,
+    paddingRight: 9,
+  },
+  Frame17: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  Txt694: {
+    fontSize: 12,
+    fontFamily: 'Red Hat Display, sans-serif',
+    fontWeight: '500',
+    letterSpacing: 0.2,
+    color: 'rgba(219,1,255,1)',
+  },
+  Rectangle4191: {
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    width: '90%',
+    height: '80%',
+    borderRadius: 15,
+    // justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  Image2: {
+    width: 95,
+    height: 96,
+  },
+});
